04ac0816f863f2b13ba367a340a93542f4b79232
Luan Vasconcelos <luan@acone.com.br>
2022-02-28 20:48:05 -0300

    Hashing password

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,21 @@
-import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
 import UsersController from '../Controllers/UsersController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
 usersRouter.get('/', usersController.index);
+
+usersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.show,
+);
 
 usersRouter.post(
   '/',
@@ -17,6 +27,31 @@ usersRouter.post(
     },
   }),
   usersController.create,
+);
+
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.update,
+);
+
+usersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.delete,
 );
 
 export default usersRouter;
