0eb8c79dd7cecd59bb875f6eaee7926f36acf942
Luan Vasconcelos <luan@acone.com.br>
2022-02-28 18:20:33 -0300

    Routes do módulo de Users

---
+++ src/modules/users/Controllers/UsersController.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import DeleteUserService from '../services/DeleteUserService';
+import ListUserService from '../services/ListUserService';
+import ShowUserService from '../services/ShowUserService';
+import UpdateUserService from '../services/UpdateUserService';
+
+export default class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUsers = new ListUserService();
+
+    const products = await listUsers.execute();
+
+    return response.json(products);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showUser = new ShowUserService();
+
+    const product = await showUser.execute({ id });
+
+    return response.json(product);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const product = await createUser.execute({ name, price, quantity });
+
+    return response.json(product);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+    const { id } = request.params;
+    const updateUser = new UpdateUserService();
+
+    const product = await updateUser.execute({ id, name, email, password });
+
+    return response.json(product);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteUser = new DeleteUserService();
+
+    await deleteUser.execute({ id });
+
+    return response.json([]);
+  }
+}
---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,57 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import UsersController from '../Controllers/UsersController';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+
+usersRouter.get('/', usersController.index);
+
+usersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.show,
+);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.update,
+);
+
+usersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.delete,
+);
+
+export default usersRouter;
