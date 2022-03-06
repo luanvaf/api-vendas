08d7ff2c27795780c21ea8037bc71605030fcdc8
Luan Vasconcelos <luan@acone.com.br>
2022-03-06 18:16:20 -0300

    User Profile

---
+++ src/modules/users/routes/profile.routs.ts
@@ -0,0 +1,32 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import isAutenticated from '../../../shared/http/middlewares/isAutenticated';
+import ProfileController from '../Controllers/ProfileController';
+
+const profileRouter = Router();
+const profileController = new ProfileController();
+
+profileRouter.use(isAutenticated);
+
+profileRouter.get('/', profileController.show);
+
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string().optional(),
+      password_confirmation: Joi.string()
+        .valid(Joi.ref('password'))
+        .when('password', {
+          is: Joi.exist(),
+          then: Joi.required(),
+        }),
+    },
+  }),
+  profileController.update,
+);
+
+export default profileRouter;
